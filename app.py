98fc5c33c0dccd0f8e21eee57ece1378827b03cd
Nisreen Hisham <157918418+nhi-vuega@users.noreply.github.com>
2024-07-12 15:17:03 +0300

    for publishing

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def index():
     return render_template('app.html', api_key=api_key)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
